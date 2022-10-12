384d09057a1bf5ca9c4ddfa7e6fc5967fcb2993f
prince nyarko <nyankonton122@gmail.com>
2022-10-12 11:32:50 -0300

    new file:   Screenshot_20221012_093918.png 	new file:   chamada_Trabalho01 	modified:   chamada_Trabalho01.c

--- chamada_Trabalho01.c
+++ chamada_Trabalho01.c
@@ -5,6 +5,7 @@ int main(void) {
 
     const char *msg = "Ola, mundo!\n";
 
+    // Usamos chamada de sistema "write" que é para escrive na tela "Olá moneda".
     write(STDOUT_FILENO, msg, 13);
     return 0;
 
