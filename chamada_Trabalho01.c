60666cb670e4c795a0c33d5bfa185ab2975d6ee4
JD3an <89491859+JD3an@users.noreply.github.com>
2022-10-12 14:15:32 -0300

    Update chamada_Trabalho01.c

--- chamada_Trabalho01.c
+++ chamada_Trabalho01.c
@@ -5,7 +5,7 @@ int main(void) {
 
     const char *msg = "Ola, mundo!\n";
 
-    // Usamos chamada de sistema "write" que é para escrive na tela "Olá moneda".
+    // Usamos chamada de sistema "write" que é para escrever na tela "Ola, mundo".
     write(STDOUT_FILENO, msg, 13);
     return 0;
 
